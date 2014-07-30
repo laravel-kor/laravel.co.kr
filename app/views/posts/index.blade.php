4c9e157be9c988e14baa68fa89fa2746714496f5
moon0326 <moon.kyong@demandmedia.net>
2014-07-29 22:49:58 -0700

    updating css

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -32,12 +32,12 @@
         @endforeach
       </tbody>
     </table>
-    <div class="pull-right">
-    {{ $posts->links() }}
-    </div>
     <ul class="nav nav-pills">
       <li class="pull-right"><a href="{{ URL::to('posts/' . $category . '/new') }}"><i class="icon-pencil"></i> 글쓰기</a></li>
     </ul>
+    <div class="pull-right">
+    {{ $posts->links() }}
+    </div>
   </div>
 </div>
 
