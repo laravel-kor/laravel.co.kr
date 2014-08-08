46adc5799a59272cb937d4ae746b55e654027f5c
Kaden Lee <thisiskaden@gmail.com>
2014-08-08 19:06:10 +0900

    remove changlog and github link on footer

--- app/views/layouts/includes/footer.blade.php
+++ app/views/layouts/includes/footer.blade.php
@@ -1,3 +1,3 @@
 <footer>
-    <p>&copy; 2013 Laravel Korean User Community <a href="https://github.com/thisiskaden/laravel-korea">GitHub</a> · <a href="{{ URL::to('changelog') }}">change log</a></p>
+    <p>&copy; {{ date('Y') }} Laravel Korean User Community</p>
 </footer>
